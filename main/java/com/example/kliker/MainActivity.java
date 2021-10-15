68959c71958c4ad1fb9b2cdb8ad09d92b1aff657
SergayAgafonov <77583940+SergayAgafonov@users.noreply.github.com>
2021-10-15 19:32:40 +0300

    Add files via upload

---
+++ main/java/com/example/kliker/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.kliker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Integer counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Counter", counter);
+    }
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("Counter");
+        resetUI();
+    }
+
+    private void resetUI() {
+        ((TextView) findViewById(R.id.txt_counter)).setText(counter.toString());
+
+    }
+
+    public void onClickBtnAddStudents(View view) {
+        counter++;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+    public void onClickBtnDellStudents(View view) {
+        counter = 0;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+}
